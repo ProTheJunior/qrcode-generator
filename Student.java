2476a65bb118cefcceb25dc3fc09eb8c32b4bb64
Prabhudutta Baral <prabhuduttabaral920@gmail.com>
2025-01-23 22:33:15 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,81 @@
+package com.pro.QrCodeModel;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+
+
+@Entity
+
+
+
+public class Student {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+	private String email;
+	private int mobile;
+	
+
+	    // No-argument constructor (required by Hibernate/JPA)
+	    public Student() {
+	        // Default constructor for JPA
+	    }
+
+	    // Parameterized constructor (optional, used for ease of object creation)
+	    public Student(long id, String firstName, String lastName, String email, int mobile) {
+	        this.id = id;
+	        this.firstName = firstName;
+	        this.lastName = lastName;
+	        this.email = email;
+	        this.mobile = mobile;
+	    }
+
+	    // Getters and setters for the fields
+	    public long getId() {
+	        return id;
+	    }
+
+	    public void setId(long id) {
+	        this.id = id;
+	    }
+
+	    public String getFirstName() {
+	        return firstName;
+	    }
+
+	    public void setFirstName(String firstName) {
+	        this.firstName = firstName;
+	    }
+
+	    public String getLastName() {
+	        return lastName;
+	    }
+
+	    public void setLastName(String lastName) {
+	        this.lastName = lastName;
+	    }
+
+	    public String getEmail() {
+	        return email;
+	    }
+
+	    public void setEmail(String email) {
+	        this.email = email;
+	    }
+
+	    public int getMobile() {
+	        return mobile;
+	    }
+
+	    public void setMobile(int mobile) {
+	        this.mobile = mobile;
+	    }
+	}
+
+	
